8f03bc7ff35d85e790890627bb8696fb6f8fe828
Penny Tague <pmtague9@gmail.com>
2019-06-19 16:04:54 -0700

    Added menu slide in from left

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,16 +1,21 @@
+// Toggle the "menu--open" class on your menu reference
+// Also Added Menu Slide-in From the Left
 
 const toggleMenu = () => {
-  menu.classList.toggle('menu--open')
-  // Toggle the "menu--open" class on your menu reference. 
+  menu.classList.toggle('menu--open');
+  TweenMax.from(menu, 1, {left: -350});
+  TweenMax.to(menu, 1, {left: 0});
+  // TweenMax.to(menu, 1, {left: -350, delay: 0.5});
 }
 
 // Start Here: Create a reference to the ".menu" class
+
 const menu = document.querySelector('.menu');
-console.log(menu)
+
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', () => {
   toggleMenu()
 });
-  
+
